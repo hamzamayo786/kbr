dc8650129ac3c0c6a332f8d17061893cf110005f
hamzamayo786 <hamza108mayo@gmail.com>
2024-04-06 22:28:29 +0500

    job pages new changes

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -12,7 +12,9 @@ const Header = () => {
                 
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container">
-                        <a class="navbar-brand" href="#">LOGO</a>
+                        <a class="navbar-brand" href="#">
+                            <img className="main-logo " src="../../../../assets/images/main-logo.png" alt="KBR team" />
+                        </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>

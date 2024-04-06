9ad05efe3fe17e1eace8b6e91cddd0eb20956edf
Wasif-Raza1 <wasifraza8011@gmail.com>
2024-04-06 20:37:12 +0500

    kbr-ww

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import '../home/css/home.css';
 import Banner from "./banner";
 import Detail from "./detail";
-import '../../assets/js/main.js'
 
 const Home = () => {
     return (
